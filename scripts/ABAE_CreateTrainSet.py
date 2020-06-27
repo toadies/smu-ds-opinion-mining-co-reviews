813dc285b6e95a03c5ef6b35b1e0f38c343bdd51
Christopher Ballenger <chris.ballenger@gmail.com>
2020-06-26 22:50:32 -0500

    Update preprocess step and add a seperate train_x for abae

--- scripts/ABAE_CreateTrainSet.py
+++ scripts/ABAE_CreateTrainSet.py
@@ -10,61 +10,44 @@ import pickle
 from tqdm import tqdm
 import multiprocessing as mp
 from multiprocessing import Pool
-from nltk import pos_tag
 import ABAE.reader as dataset
-
-
-def filterWords(sent):
-    tags = ["N","J","R"]
-    tokens = sent.split()
-    pos_tokens = pos_tag(tokens)
-    tokens = [ word[0] for word in pos_tokens if word[1][0] in tags ]
-    return " ".join(tokens)
-
-
-with open(os.path.join(project_path,"data/stop_words.json"), "r") as f:
-  stop_words = json.load(f)
-
+import numpy as np
+import json
 
 def removeStopWords(review):
-  tokens = review.split(" ")
-  return " ".join([ word for word in tokens if word not in stop_words ])
+    tokens = review.split(" ")
+    return " ".join([ word for word in tokens if word not in stop_words ])
+
 
 if __name__ == "__main__":
-    review_corpus_path = os.path.join(project_path, "data/tech_review_sent_corpus.pkl")
-
-    with open(review_corpus_path, "rb") as f:
+    with open(os.path.join(project_path, "data/tech_review_sent_corpus.pkl"),"rb") as f:
         tech_review_corpus = pickle.load(f)
 
-    indices = [review["index"] for review in tech_review_corpus]
+    print("Initial Corpus Size", len(tech_review_corpus))
+
+    with open(os.path.join(project_path, "data/stop_words.json"), "r") as f:
+        stop_words = json.load(f)
+
     reviews = [review["review"] for review in tech_review_corpus]
 
-    print("Parse Reviews")
     with Pool() as p:
-        reviews = list(tqdm(p.imap(filterWords, reviews), total=len(reviews)))
         reviews = list(tqdm(p.imap(removeStopWords, reviews), total=len(reviews)))
 
-    tech_review_sent_corpus_nn_adj_adv = [ {"index":idx, "review":sent}
-                                 for idx, sent in zip(indices, reviews) if len(sent.split()) > 1]
+    word_length = [len(review) for review in reviews]
 
-    print("Final Size", len(tech_review_sent_corpus_nn_adj_adv))
+    print("Max Character length", max(word_length))
+    print("Average Character Length", np.mean(word_length))
+    print("Standard Deviation", np.std(word_length))
+    print("Median Character Length", np.median(word_length))
+    print("Words 2 standard deviations from mean", np.mean(word_length) + (2*np.std(word_length)))
+    
+    maxlen = int(round(np.mean(word_length) + (2*np.std(word_length)))) #Based on 2 standard deviations from mean
 
-    with open(os.path.join(project_path, "data/tech_review_sent_corpus_nn_adj_adv.pkl"), "wb") as f:
-        pickle.dump(tech_review_sent_corpus_nn_adj_adv, f)
+    vocab, train_x, overall_maxlen = dataset.get_data(reviews, maxlen=maxlen, word_freq=5)
 
-    tech_review_word_corpus = {review["index"]: [] for review in tech_review_sent_corpus_nn_adj_adv}
-    for review in tech_review_sent_corpus_nn_adj_adv:
-        tech_review_word_corpus[review["index"]].append(review["review"])
+    # train_x = train_x[0:30000]
+    print('Number of training examples: ', len(train_x))
+    print('Length of vocab: ', len(vocab))
 
-    tech_review_word_corpus_nn_adj_adv = []
-    for key, value in tech_review_word_corpus.items():
-        tech_review_word_corpus_nn_adj_adv.append({
-            "index":key,
-            "review":" ".join(value)
-        })
-
-    print("Total Size of Reviews", len(tech_review_word_corpus_nn_adj_adv))
-    with open(os.path.join(project_path, "data/tech_review_word_corpus_nn_adj_adv.pkl"), "wb") as f:
-        pickle.dump(tech_review_word_corpus_nn_adj_adv, f)
-
-
+    with open(os.path.join(project_path, "data/abae_train.pkl"),"wb") as f:
+        pickle.dump((vocab, train_x, overall_maxlen), f)
